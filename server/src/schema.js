d9c8d2fb39ac9c05afe85a6060ac42451679b08f
Therin Mody <therin.mody@edu.sait.ca>
2022-01-18 12:28:30 -0800

    Completed Lift-off part 3: Arguments

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,21 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The tracks complete description"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The list of modules"
+    modules: [Module!]!
+  }
+
+  "A module is a single unit of teaching. Multiple Modules compose a track"
+  type Module {
+    id: ID!
+    "The Modules title"
+    title: String!
+    "The Modules length in minutes"
+    length: Int
   }
 
   "Author of a complete Track or a Module"
